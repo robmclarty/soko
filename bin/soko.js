986ea54565f781324a60b5a087d86a7847207b48
slode <rob.m@myplanet.com>
2017-12-05 12:54:30 -0500

    test removing gulp from equation for server task

---
+++ bin/soko.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const shell = require('shelljs')
+const argv = require('minimist')(process.argv.slice(2))
+
+const tasks = {
+  server: require('../tasks/server')
+}
+
+// Takes `argv` from yargs and returns the args formatted as a string for the cli.
+// ref: https://github.com/yargs/yargs
+const argvToString = a => {
+  return Object.keys(a).reduce((acc, key) => {
+    if (key === '_') return acc
+
+    if (a[key] === true) return acc + ` --${ key }`
+
+    if (a[key] === false) return acc + ` --no-${ key }`
+
+    return acc + ` --${ key } ${ a[key] }`
+  }, '')
+}
+
+const action = argv._[0]
+// const cmd = `${} ${ action } ${ argvToString(argv) }`
+//
+// shell.exec(cmd)
+
+tasks[action](argv)
--- tasks/server.js
+++ tasks/server.js
@@ -1,18 +1,36 @@
 'use strict'
 
-const gulp = require('gulp')
-const argv = require('yargs').argv
+//const gulp = require('gulp')
+//const argv = require('yargs').argv
 const spawn = require('child_process').spawn
 
-const DEFAULT_PATH = './server/index.js'
+const DEFAULT_PATH = './index.js'
 
 let node
 
 // Launch the server. If there's a server already running, kill it.
 // Inspired by https://gist.github.com/webdesserts/5632955
-gulp.task('server', function (done) {
+// gulp.task('server', function (done) {
+//   console.log('args: ', argv)
+//   const path = argv.path || DEFAULT_PATH
+//
+//   if (node) node.kill()
+//
+//   process.env.NODE_ENV = 'development'
+//   node = spawn('node', [path], { stdio: 'inherit' })
+//
+//   node.on('close', code => {
+//     if (code === 8) glup.log('Error detected, waiting for changes...')
+//   })
+//
+//   done()
+// })
+
+const server = argv => {
+  const path = argv._[1] || DEFAULT_PATH
+
   console.log('args: ', argv)
-  const path = argv.path || DEFAULT_PATH
+  //const path = argv.path || DEFAULT_PATH // TODO: change this to simply be the first argument
 
   if (node) node.kill()
 
@@ -20,13 +38,13 @@ gulp.task('server', function (done) {
   node = spawn('node', [path], { stdio: 'inherit' })
 
   node.on('close', code => {
-    if (code === 8) glup.log('Error detected, waiting for changes...')
+    if (code === 8) console.log('Error detected, waiting for changes...')
   })
-
-  done()
-});
+}
 
 // Clean up if an error goes unhandled.
 process.on('exit', function () {
   if (node) node.kill()
 })
+
+module.exports = server
